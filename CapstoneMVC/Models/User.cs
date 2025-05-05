95b83f985840d84a3290f55c7bfab5d1b081cdcf
Rayleen Roberts <rayleen.roberts@student.centralia.edu>
2025-05-04 22:12:53 -0700

    Add project files.

---
+++ CapstoneMVC/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapstoneMVC.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "UserType is Required")]
+        public int UserType { get; set; }
+        [Required(ErrorMessage = "IsConfidential is Required")]
+        public bool IsConfidential { get; set; }
+    }
+}
